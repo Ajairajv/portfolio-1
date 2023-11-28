4d7b6d882a6f3d4779828e7af3bff57f6e3aa414
Ajai <armmjvs@gmail.com>
2023-11-28 23:41:20 +0530

    update
    png changing in hero

--- components/Herosection.tsx
+++ components/Herosection.tsx
@@ -9,7 +9,7 @@ const Herosection = () => {
    <section id="home">
     <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
-            <Image src="/head.jpg" alt="" 
+            <Image src="/head.png" alt="" 
              width={325}
              height={325}
              className="rounded-full shadow-2xl"
